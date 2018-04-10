9929b6a12ebd17f22e85a51f13f648eff0eb2842
Adomas1237 <31615497+Adomas1237@users.noreply.github.com>
2018-04-11 01:01:42 +0300

    api

--- manofthematch/api/ContentController.cs
+++ manofthematch/api/ContentController.cs
@@ -48,21 +48,62 @@ namespace manofthematch.Core.Controllers.WebAPI
             //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
             //var k = umbHelper.Media(smt);
             //result.Name = k.Url;
-            
+
             //looping children of sports node
             //foreach (var item in content)
             //{   
             //    var j 
-                //obj.key = j;
-            
-           
+            //obj.key = j;
 
-                
             //}
+            //make the selected players neat, egzample "8676e6d1b9134bc3823ea5242f5d63e3"
+            var j = content.GetValue<string>("playerpicker");
+            
+            string[] tokens = j.Split(',');
+            var token = tokens[1].Replace("umb://document/","");
+            //var smt = Umbraco.GetIdForUdi(j[0]);
+            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+            //var m = tokens[0]; 
+            //var k = umbHelper.TypedContent(m);
 
+            //get all players
+            var temp = cs.GetChildren(1088);
 
-            string output = JsonConvert.SerializeObject(content);
+            foreach (var item in temp)
+            {
+                var id = item.Id;
+                var key = item.Key.ToString();
+                key = key.Replace("-", "");
+                result.Id = id;
+                result.key = key;
+            }
 
+            var hij = 0;           
+            var root = token;
+            var root2 = result.key;
+           
+            bool result1 = root.Equals(root2);
+            int comparison = root.CompareTo(root2);
+            result1 = root.Equals(root2, StringComparison.Ordinal);
+            if (result1 == true)
+            {
+                hij = result.Id;
+            }
+            //result.Name = k;
+
+            var smt = cs.GetById(hij);
+            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+            //var m = tokens[0]; 
+            //var k = umbHelper.TypedContent(m);
+
+            // var smt = Umbraco.GetIdForUdi(j);
+            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+            // var k = umbHelper.Media(smt);
+            // result.Name = k.Url;
+
+            var output = JsonConvert.SerializeObject(smt);
+
+           
 
             return output;
 
@@ -71,8 +112,9 @@ namespace manofthematch.Core.Controllers.WebAPI
       
         public class result
         {
-         
-         
+            public int Id { get; set; }
+            public string key { get; set; }
+
             public object Name { get; set; }
          
         }
