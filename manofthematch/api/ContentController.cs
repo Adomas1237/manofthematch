a43e46f26ef3b84c2ce56293c2601aaaf8627221
Adomas1237 <31615497+Adomas1237@users.noreply.github.com>
2018-04-11 23:54:36 +0300

    api

--- manofthematch/api/ContentController.cs
+++ manofthematch/api/ContentController.cs
@@ -23,85 +23,26 @@ namespace manofthematch.Core.Controllers.WebAPI
             result result = new result();
             var cs = Services.ContentService;
             // getting children of sports
-            var content = cs.GetById(1091);
+            var content = cs.GetChildren(1076);
             
             dynamic obj = new JObject();
-
-            //example place
-
-            //nesting content
-            //obj.key2 = "value2";
-            //obj.key1 = new JObject
-            //{
-            //    {"key3", "asdaddd" }
-            //};
-            //obj.key1.key3 = new JObject
-            //{
-            //    {"key4", "dddddd" }
-            //};
-            //result.Name = item.GetValue("imagepost");
-
-
-            //getting images
-            //var j = item.GetValue<Udi>("imagepost");
-            //var smt = Umbraco.GetIdForUdi(j);
-            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
-            //var k = umbHelper.Media(smt);
-            //result.Name = k.Url;
-
-            //looping children of sports node
-            //foreach (var item in content)
-            //{   
-            //    var j 
-            //obj.key = j;
-
-            //}
-            //make the selected players neat, egzample "8676e6d1b9134bc3823ea5242f5d63e3"
-            var j = content.GetValue<string>("playerpicker");
-            
-            string[] tokens = j.Split(',');
-            var token = tokens[1].Replace("umb://document/","");
-            //var smt = Umbraco.GetIdForUdi(j[0]);
-            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
-            //var m = tokens[0]; 
-            //var k = umbHelper.TypedContent(m);
-
-            //get all players
-            var temp = cs.GetChildren(1088);
-
-            foreach (var item in temp)
+            int i = 0;
+           foreach( var item in content)
             {
-                var id = item.Id;
-                var key = item.Key.ToString();
-                key = key.Replace("-", "");
-                result.Id = id;
-                result.key = key;
+                var j = item.GetValue<Udi>("imagepost");
+                var smt = Umbraco.GetIdForUdi(j);
+                var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+                var k = umbHelper.Media(smt);
+                obj["sport"+i] = new JObject
+                {
+                    { "SportName", item.GetValue<string>("namesport")  },
+                    { "SportPic", k.Url },
+
+                };
+                i++;
             }
 
-            var hij = 0;           
-            var root = token;
-            var root2 = result.key;
-           
-            bool result1 = root.Equals(root2);
-            int comparison = root.CompareTo(root2);
-            result1 = root.Equals(root2, StringComparison.Ordinal);
-            if (result1 == true)
-            {
-                hij = result.Id;
-            }
-            //result.Name = k;
-
-            var smt = cs.GetById(hij);
-            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
-            //var m = tokens[0]; 
-            //var k = umbHelper.TypedContent(m);
-
-            // var smt = Umbraco.GetIdForUdi(j);
-            //var umbHelper = new UmbracoHelper(UmbracoContext.Current);
-            // var k = umbHelper.Media(smt);
-            // result.Name = k.Url;
-
-            var output = JsonConvert.SerializeObject(smt);
+            var output = JsonConvert.SerializeObject(obj);
 
            
 
@@ -120,3 +61,66 @@ namespace manofthematch.Core.Controllers.WebAPI
         }
     }
 }
+//example place
+
+//nesting content
+//obj.key2 = "value2";
+//obj.key1 = new JObject
+//{
+//    {"key3", "asdaddd" }
+//};
+//obj.key1.key3 = new JObject
+//{
+//    {"key4", "dddddd" }
+//};
+//result.Name = item.GetValue("imagepost");
+
+
+//getting images
+//var j = item.GetValue<Udi>("imagepost");
+//var smt = Umbraco.GetIdForUdi(j);
+//var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+//var k = umbHelper.Media(smt);
+//result.Name = k.Url;
+
+//looping children of sports node
+//foreach (var item in content)
+//{   
+//    var j 
+//obj.key = j;
+
+//}
+
+//get the players from player picker
+//make the selected players neat, egzample "8676e6d1b9134bc3823ea5242f5d63e3"
+//var j = content.GetValue<string>("playerpicker");
+
+//string[] tokens = j.Split(',');
+//var token = tokens[1].Replace("umb://document/","");
+
+
+////get all players
+//var temp = cs.GetChildren(1088);
+
+//foreach (var item in temp)
+//{
+//    var id = item.Id;
+//    var key = item.Key.ToString();
+//    key = key.Replace("-", "");
+//    result.Id = id;
+//    result.key = key;
+//}
+
+//var hij = 0;           
+//var root = token;
+//var root2 = result.key;
+
+//bool result1 = root.Equals(root2);
+//int comparison = root.CompareTo(root2);
+//result1 = root.Equals(root2, StringComparison.Ordinal);
+//if (result1 == true)
+//{
+//    hij = result.Id;
+//}
+
+//var smt = cs.GetById(hij);
